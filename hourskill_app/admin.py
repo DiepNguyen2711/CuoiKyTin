2046322e556e8c3f027d74a30abac0fcd4d1ac26
DiepNguyen2711 <diepnodk@gmail.com>
2026-02-26 21:15:52 +0700

    Update khảo sát user về vai trò (giáo viên, sinh viên ngành...) phần backend

--- hourskill_app/admin.py
+++ hourskill_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.apps import apps
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import UserProfile
 
 # Tùy chỉnh tiêu đề trang quản trị cho xịn sò
 admin.site.site_header = "Hệ thống Quản trị HourSkill"
@@ -13,3 +16,25 @@ for model in app_models:
         admin.site.register(model)
     except admin.sites.AlreadyRegistered:
         pass
+
+
+# Check khảo sát của các user
+# Tạo một block hiển thị cho UserProfile
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Thông tin Onboarding (Vai trò & Khảo sát)'
+
+# Tạo một UserAdmin mới, kế thừa cái cũ và nhét block UserProfile vào
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserProfileInline,)
+
+# Hủy đăng ký bảng User cũ của Django và đăng ký lại bằng bảng User mới đã được nâng cấp
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+#  Mục UserProfile đứng riêng lẻ bên ngoài menu chính
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role') # Hiển thị 2 cột này ở ngoài danh sách
+    search_fields = ('user__username', 'user__email', 'role') # Thêm thanh tìm kiếm
