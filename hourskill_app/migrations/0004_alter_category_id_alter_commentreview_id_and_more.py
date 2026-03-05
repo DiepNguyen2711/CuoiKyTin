a7d903d5a2623b4433e6df391a9e3fc303f95349
DiepNguyen2711 <diepnodk@gmail.com>
2026-03-05 23:42:23 +0700

    Revert "Merge pull request #76 from DiepNguyen2711/restore-upload-video"
    This reverts commit 4b21f39ebb50443cfd94a703d7952d2ac76c3714, reversing
    changes made to 4866fb5e775ffa67b97104f7fd158afab386eab7.

--- hourskill_app/migrations/0004_alter_category_id_alter_commentreview_id_and_more.py
+++ hourskill_app/migrations/0004_alter_category_id_alter_commentreview_id_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('hourskill_app', '0002_alter_video_duration_seconds'),
+        ('hourskill_app', '0003_merge_20260305_0139'),
     ]
 
     operations = [
